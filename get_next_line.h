1fd4ab85d303274bc3ffa8ab526ae3307230cee0
yubi42 <133892327+yubi42@users.noreply.github.com>
2023-06-02 16:14:35 +0200

    updated version of the header file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jborner <jborner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 12:53:02 by jborner           #+#    #+#             */
-/*   Updated: 2023/05/29 19:03:20 by jborner          ###   ########.fr       */
+/*   Updated: 2023/05/31 13:49:18 by jborner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,15 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 20
+#  define BUFFER_SIZE 10
 # endif
 
 char	*read_data(int fd, char *buffer, char *str, int *j);
+char	*file_end(int bytes_read, int *j, char *str, char *buffer);
 char	*str_next_line(int fd, char *buffer, char *str, int *j);
 char	*get_next_line(int fd);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_my_realloc(void *str, size_t size);
+size_t	ft_strlen(const char *s);
 
 #endif
